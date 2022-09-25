20b56494e3e95b0660df9a8e6feb893f6a8cc19e
heiker.gil <gilheyker@gmail.com>
2022-09-25 09:43:10 -0500

    conditionals

--- e2e/tests/local/buildFlow/BuildflowPricing.spec.ts
+++ e2e/tests/local/buildFlow/BuildflowPricing.spec.ts
@@ -20,62 +20,7 @@ test.describe.serial('Eyeglasses Buildflow pricing @buildflow', async () => {
 	})
 
 	test('adding tops should update price correctly @buildflow', async ({ }) => {
-		const prices = buildFlow.prices;
-		await buildFlow.goToChooseTopFrames();
-		await buildFlow.handlePopUp();
-		await buildFlow.clickChooseTopFramesBtn();
-		expect(await buildFlow.returnBuildFlowSubtotalValue()).toBe(prices['baseFrame']);
-		expect(await buildFlow.returnBuildFlowPaymentsValue()).toBe((await buildFlow.returnBuildFlowSubtotalValue()) / 4);
-		const topIndex = await buildFlow.addTop();
-		const topPrice = await buildFlow.returnTopPrice(topIndex)
-		expect(await buildFlow.returnBuildFlowSubtotalValue()).toBe(prices['baseFrame'] + topPrice);
-		expect(await buildFlow.returnBuildFlowPaymentsValue()).toBe((await buildFlow.returnBuildFlowSubtotalValue()) / 4);
-		await buildFlow.currentCollectionsTopsBtn.nth(topIndex).click();
-	
-		expect(await buildFlow.returnBuildFlowSubtotalValue()).toBe(prices['baseFrame']);
-		expect(await buildFlow.returnBuildFlowPaymentsValue()).toBe((await buildFlow.returnBuildFlowSubtotalValue()) / 4);
-	
+		expect(0).toBe(1)
 	});
 
-	test('Customer Should see the correct pricing when selecting Single-Vision', async ({}) => {
-		await buildFlow.clickSelectLensTypeBtn();
-		await buildFlow.clickSingleVision();
-		expect(await buildFlow.returnBuildFlowSubtotalValue()).toBe(prices['baseFrame']);
-		expect(await buildFlow.returnBuildFlowPaymentsValue()).toBe((await buildFlow.returnBuildFlowSubtotalValue()) / 4);
-		await buildFlow.clickPremiunPlus();
-		await buildFlow.clickBlueLightFiltering();
-		await buildFlow.clickLightResponsive();
-		expect(await buildFlow.returnBuildFlowSubtotalValue()).toBe(
-			prices['baseFrame'] + prices['premiunPlus'] + prices['blueLightFiltering'] + prices['lightResponsive']);
-		expect(await buildFlow.returnBuildFlowPaymentsValue()).toBe((await buildFlow.returnBuildFlowSubtotalValue()) / 4);
-		await buildFlow.clickBackBtn();
-	});
-
-	test('Customer Should see the correct pricing when selecting Progressives', async ({}) => {
-		await buildFlow.clickProgressive();
-		expect(await buildFlow.returnBuildFlowSubtotalValue()).toBe(
-			prices['baseFrame'] +
-				prices['premiunPlus'] +
-				prices['blueLightFiltering'] +
-				prices['lightResponsive'] +
-				prices['progressives']
-		);
-		expect(await buildFlow.returnBuildFlowPaymentsValue()).toBe((await buildFlow.returnBuildFlowSubtotalValue()) / 4);
-		await buildFlow.clickBackBtn();
-	});
-
-	test('Customer Should see the correct pricing when selecting Readers', async ({}) => {
-		await buildFlow.clickReaders();
-		expect(await buildFlow.returnBuildFlowSubtotalValue()).toBe(
-			prices['baseFrame'] + prices['blueLightFiltering'] + prices['lightResponsive']);
-		expect(await buildFlow.returnBuildFlowPaymentsValue()).toBe((await buildFlow.returnBuildFlowSubtotalValue()) / 4);
-		await buildFlow.clickBackBtn();
-	});
-
-	test('Customer Should see the correct pricing when selecting Non-Prescription', async ({}) => {
-		await buildFlow.clickNonPrescription();
-		expect(await buildFlow.returnBuildFlowSubtotalValue()).toBe(
-			prices['baseFrame'] + prices['blueLightFiltering'] + prices['lightResponsive']);
-		expect(await buildFlow.returnBuildFlowPaymentsValue()).toBe((await buildFlow.returnBuildFlowSubtotalValue()) / 4);
-	});
 });
